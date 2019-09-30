d87150c230301851cb10e8f882398eaca6482221
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-30 14:38:29 -0600

    feat: initial release

---
+++ css-in-js/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["stylelint-config-starstuff"],
+  rules: {
+    "declaration-empty-line-before": [
+      "never",
+      {
+        ignore: [
+          "after-comment",
+          "after-declaration",
+          "inside-single-line-block"
+        ]
+      }
+    ]
+  }
+};
