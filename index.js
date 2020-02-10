7434d910097dbc66f6648193905eaffe48b3d855
Kepler Sticka-Jones <kepler+git@stickajones.org>
2020-02-09 19:52:39 -0700

    feat: include accesibility rules

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: [
     "stylelint-config-recommended",
     "stylelint-config-standard",
+
+    "stylelint-a11y/recommended",
+
     "stylelint-prettier/recommended"
   ]
 };
